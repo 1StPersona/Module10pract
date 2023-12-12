6e6180215cd665a5261e877f1bf8432d3166f42f
Danila Zakharov <49668471+1StPersona@users.noreply.github.com>
2023-12-12 18:15:42 +0600

    Добавьте файлы проекта.

---
+++ Module10pract/House.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10pract
+{
+    public class House
+    {
+        private List<IPart> parts = new List<IPart>();
+
+        public void AddPart(IPart part)
+        {
+            parts.Add(part);
+        }
+
+        public void ShowParts()
+        {
+            Console.WriteLine("House parts:");
+            foreach (var part in parts)
+            {
+                Console.WriteLine(part.Name);
+            }
+        }
+    }
+}
---
+++ Module10pract/Roof.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10pract
+{
+    public class Roof : IPart
+    {
+        public string Name => "Roof";
+    }
+}
